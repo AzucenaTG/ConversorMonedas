ed12665ecc4c314aa860cde34122fc6b0ae98c03
Azucena <azucenacontacto@gmail.com>
2022-12-23 21:30:01 -0300

    Actualizado y terminado

--- src/conversorDeMonedas/conversor.java
+++ src/conversorDeMonedas/conversor.java
@@ -1,4 +1,5 @@
 package conversorDeMonedas;
+
 import javax.swing.JOptionPane;
 
 public class conversor {
@@ -35,6 +36,124 @@ public class conversor {
 
 		}
 	}
-	
-	
+
+	static void convertirPesosAEuros(double valorEuros, String tuPais) {
+		try {
+			String mensajeEnPantalla = JOptionPane.showInputDialog(null, "Ingrese la cantidad de pesos :");
+			double cantidadDeMoneda = Double.parseDouble(mensajeEnPantalla);
+			double euros = cantidadDeMoneda / valorEuros;
+			euros = (double) Math.round(euros * 100d) / 100;
+
+			JOptionPane.showMessageDialog(null, "tienes $ " + euros + " Euros");
+		} catch (NumberFormatException exception) {
+			JOptionPane.showMessageDialog(null, "Ocurrio un error, se acepta solamente caracter numerico");
+			System.out.println("error captado");
+
+		}
+	}
+
+	static void convertirEurosAPesos(String tuPais, double valorEuros) {
+		try {
+			String mensajeEnPantalla = JOptionPane.showInputDialog(null, "Ingrese la cantidad de euros :");
+			double cantidadDeMoneda = Double.parseDouble(mensajeEnPantalla);
+			double pesos = valorEuros / cantidadDeMoneda;
+			pesos = (double) (cantidadDeMoneda * 185.58);
+
+			JOptionPane.showMessageDialog(null, "tienes $ " + pesos + " Pesos");
+		} catch (NumberFormatException exception) {
+			JOptionPane.showMessageDialog(null, "Ocurrio un error, se acepta solamente caracter numerico");
+			System.out.println("error captado");
+
+		}
+	}
+
+	static void convertirPesosALibrasEsterlinas(double valorLibrasEsterlinas, String tuPais) {
+		try {
+			String mensajeEnPantalla = JOptionPane.showInputDialog(null, "Ingrese la cantidad de pesos :");
+			double cantidadDeMoneda = Double.parseDouble(mensajeEnPantalla);
+			double librasEsterlinas = cantidadDeMoneda / valorLibrasEsterlinas;
+			librasEsterlinas = (double) Math.round(librasEsterlinas * 100d) / 100;
+
+			JOptionPane.showMessageDialog(null, "tienes $ " + librasEsterlinas + " Libras Esterlinas");
+		} catch (NumberFormatException exception) {
+			JOptionPane.showMessageDialog(null, "Ocurrio un error, se acepta solamente caracter numerico");
+			System.out.println("error captado");
+
+		}
+	}
+
+	static void convertirLibrasEsterlinasAPesos(String tuPais, double valorLibrasEsterlinas) {
+		try {
+			String mensajeEnPantalla = JOptionPane.showInputDialog(null, "Ingrese la cantidad de Libras Esterlinas :");
+			double cantidadDeMoneda = Double.parseDouble(mensajeEnPantalla);
+			double pesos = valorLibrasEsterlinas / cantidadDeMoneda;
+			pesos = (double) (cantidadDeMoneda * 0.0048);
+
+			JOptionPane.showMessageDialog(null, "tienes $ " + pesos + " Pesos");
+		} catch (NumberFormatException exception) {
+			JOptionPane.showMessageDialog(null, "Ocurrio un error, se acepta solamente caracter numerico");
+			System.out.println("error captado");
+
+		}
+	}
+
+	static void convertirPesosAYenes(double valorYenes, String tuPais) {
+		try {
+			String mensajeEnPantalla = JOptionPane.showInputDialog(null, "Ingrese la cantidad de pesos :");
+			double cantidadDeMoneda = Double.parseDouble(mensajeEnPantalla);
+			double yenes = cantidadDeMoneda / valorYenes;
+			yenes = (double) Math.round(yenes * 100d) / 100;
+
+			JOptionPane.showMessageDialog(null, "tienes $ " + yenes + " Yenes");
+		} catch (NumberFormatException exception) {
+			JOptionPane.showMessageDialog(null, "Ocurrio un error, se acepta solamente caracter numerico");
+			System.out.println("error captado");
+
+		}
+	}
+
+	static void convertirYenesAPesos(String tuPais, double valorYenes) {
+		try {
+			String mensajeEnPantalla = JOptionPane.showInputDialog(null, "Ingrese la cantidad de Yenes :");
+			double cantidadDeMoneda = Double.parseDouble(mensajeEnPantalla);
+			double pesos = valorYenes / cantidadDeMoneda;
+			pesos = (double) (cantidadDeMoneda * 0.76);
+
+			JOptionPane.showMessageDialog(null, "tienes $ " + pesos + " Pesos");
+		} catch (NumberFormatException exception) {
+			JOptionPane.showMessageDialog(null, "Ocurrio un error, se acepta solamente caracter numerico");
+			System.out.println("error captado");
+
+		}
+	}
+
+	static void convertirPesosAWons(double valorWons, String tuPais) {
+		try {
+			String mensajeEnPantalla = JOptionPane.showInputDialog(null, "Ingrese la cantidad de pesos :");
+			double cantidadDeMoneda = Double.parseDouble(mensajeEnPantalla);
+			double wons = cantidadDeMoneda / valorWons;
+			wons = (double) Math.round(wons * 100d) / 100;
+
+			JOptionPane.showMessageDialog(null, "tienes $ " + wons + " Wons Surcoreano");
+		} catch (NumberFormatException exception) {
+			JOptionPane.showMessageDialog(null, "Ocurrio un error, se acepta solamente caracter numerico");
+			System.out.println("error captado");
+
+		}
+	}
+
+	static void convertirWonsAPesos(String tuPais, double valorWons) {
+		try {
+			String mensajeEnPantalla = JOptionPane.showInputDialog(null, "Ingrese la cantidad de Wons :");
+			double cantidadDeMoneda = Double.parseDouble(mensajeEnPantalla);
+			double pesos = valorWons / cantidadDeMoneda;
+			pesos = (double) (cantidadDeMoneda * 0.76);
+
+			JOptionPane.showMessageDialog(null, "tienes $ " + pesos + " Pesos");
+		} catch (NumberFormatException exception) {
+			JOptionPane.showMessageDialog(null, "Ocurrio un error, se acepta solamente caracter numerico");
+			System.out.println("error captado");
+
+		}
+	}
 }
--- src/conversorDeMonedas/mensaje.java
+++ src/conversorDeMonedas/mensaje.java
@@ -1,4 +1,5 @@
 package conversorDeMonedas;
+
 import javax.swing.JOptionPane;
 
 public class mensaje extends conversor {
@@ -6,15 +7,41 @@ public class mensaje extends conversor {
 		try {
 			// mensaje que nos permite la entrada de datos
 			String mensageEnPantalla = JOptionPane.showInputDialog(null,
-					"1- Pesos Argentinos a Dolares\n" + "2- Dolares a Pesos Argentinos\n");
+					"0- Pesos Argentinos a Dolares\n" + "1- Dolares a Pesos Argentinos\n" + "2- Pesos Argentinos a Euros\n" + "3- Euros a Pesos Argentinos\n"
+							+ "4- Pesos Argentinos a Libras Esterlinas\n" + "5- Libras Esterlinas a Pesos Argentinos\n" + "6- Pesos Argentinos a Yen Japonés\n"
+							+ "7- Yen Japonés a Pesos Argentinos\n" + "8- Pesos Argentinos a Won sur coreano\n" + "9-  Won sur coreano a Pesos Argentinos\n");
 			char opcion = mensageEnPantalla.charAt(0);
 
 			switch (opcion) {
-			case '1':
+			case '0':
 				convertirPesos(173.64, "Pesos Argentinos");
 				break;
-			case '2':
+			case '1':
 				convertirDolar("Dolares", 0.0057);
+				break;
+			case '2':
+				convertirPesosAEuros(185.58, "Pesos Argentinos");
+				break;
+			case '3':
+				convertirEurosAPesos("Pesos Argentinos", 0.0054);
+				break;
+			case '4':
+				convertirPesosALibrasEsterlinas(209.66, "Pesos Argentinos");
+				break;
+			case '5':
+				convertirLibrasEsterlinasAPesos("Pesos Argentinos", 0.0048);
+				break;
+			case '6':
+				convertirPesosAYenes(1.31, "Pesos Argentinos");
+				break;
+			case '7':
+				convertirYenesAPesos("Pesos Argentinos", 0.76);
+				break;
+			case '8':
+				convertirPesosAWons(0.14, "Pesos Argentinos");
+				break;
+			case '9':
+				convertirWonsAPesos("Pesos Argentinos", 7.36);
 				break;
 			default:
 				System.out.print("Opcion Incorrecta");
